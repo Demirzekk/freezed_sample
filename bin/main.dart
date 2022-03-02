7474a26a2dbf56dc48548cf79f3acacde7521fca
Zekai Demir <metindemir1327@gmail.com>
2022-03-02 23:58:50 +0300

    yorum

--- bin/main.dart
+++ bin/main.dart
@@ -4,5 +4,6 @@ import 'model/user.dart';
 void main( ) {
 var user= User(userId: 1, userName: "Zekk", jobs: [Job(level: 1)]); 
 print(user.toJson());
-print(user);
+print(user); 
+print("object");
 }
