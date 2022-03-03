453dec05e09b336dec4c7703518b6996b835b753
Zekai Demir <metindemir1327@gmail.com>
2022-03-03 03:09:16 +0300

    freezed

--- bin/main.dart
+++ bin/main.dart
@@ -2,20 +2,12 @@ import 'job/job.dart';
 import 'model/user.dart';
 
 void main( ) {
-/*var user= User(userId: 1, userName: "Zekk", jobs: [Job(level: 1)]); 
+var user= User(userId: 1, userName: "Zekk", jobs: [Job(level: 1)]); 
 print(user.toJson());
 print(user); 
-print("object");*/
-for(var i=0;i<10;i++){
-  if(i.isOdd){
-    print("$i");
-  }else{
-   
-     Future.delayed(Duration(seconds:  0)).then((value) => print("$i"));
-  }
-
-  
+print("object");
+ 
 }
-}
+ 
 
  
