8e212cd2b4d5ea2ea89fb157f6da6bc8da327c73
Zekai Demir <metindemir1327@gmail.com>
2022-03-03 03:01:40 +0300

    commited

--- bin/main.dart
+++ bin/main.dart
@@ -2,9 +2,20 @@ import 'job/job.dart';
 import 'model/user.dart';
 
 void main( ) {
-var user= User(userId: 1, userName: "Zekk", jobs: [Job(level: 1)]); 
+/*var user= User(userId: 1, userName: "Zekk", jobs: [Job(level: 1)]); 
 print(user.toJson());
 print(user); 
-print("object");
- 
+print("object");*/
+for(var i=0;i<10;i++){
+  if(i.isOdd){
+    print("$i");
+  }else{
+   
+     Future.delayed(Duration(seconds:  0)).then((value) => print("$i"));
+  }
+
+  
 }
+}
+
+ 
