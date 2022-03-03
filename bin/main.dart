e1011cf0831357eb199d9391d479f1b944a3b14c
Zekai Demir <metindemir1327@gmail.com>
2022-03-03 03:11:26 +0300

    == operator usage

--- bin/main.dart
+++ bin/main.dart
@@ -3,9 +3,12 @@ import 'model/user.dart';
 
 void main( ) {
 var user= User(userId: 1, userName: "Zekk", jobs: [Job(level: 1)]); 
+var user2= User(userId: 1, userName: "Zekk", jobs: [Job(level: 1)]); 
+var user3= User(userId: 10, userName: "Zek", jobs: [Job(level: 10)]); 
 print(user.toJson());
 print(user); 
-print("object");
+print(user==user2); //true
+print(user==user3); //false
  
 }
  
